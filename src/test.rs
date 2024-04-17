f0524eacbed5f6dbe14fe42495d1612047442646
forkaduck <48600374+forkaduck@users.noreply.github.com>
2024-04-17 22:41:21 +0200

    Fixed test and added comments

--- src/test.rs
+++ src/test.rs
@@ -22,6 +22,8 @@ mod tests {
         assert_eq!(data.output(input).1, rv);
     }
 
+    /// Showcases that a one-dimensional layer can't learn
+    /// an XOR Gate.
     #[test]
     fn xor_impossibility_weak() {
         setup();
@@ -44,8 +46,12 @@ mod tests {
         assert_show(&eye, &[1.0, 1.0], true);
     }
 
+    /// Shows how multiple iterations with the same data
+    /// and a nearing learning-strength can strengthen pattern recognition.
     #[test]
     fn optimizer_complex_patterns() {
+        setup();
+
         let training_data = TrainingData::try_from(vec![
             (vec![0.0, 0.0, 0.0], 0.0),
             (vec![0.0, 0.7, 0.0], 1.0),
@@ -56,7 +62,6 @@ mod tests {
             (vec![0.0, 0.0, 0.0], 0.0),
             (vec![1.0, 0.0, 1.0], 0.0),
             (vec![1.0, 1.0, 1.0], 1.0),
-            // (vec![0.0, 0.8, 0.0], 1.0),
         ])
         .unwrap();
 
@@ -76,8 +81,10 @@ mod tests {
         assert_show(&eye, &[0.8, 0.2, 0.2], false);
     }
 
+    /// Shows the anti-prove of optimizer_complex_patterns.
     #[test]
     fn optimizer_proof() {
+        setup();
         let training_data = TrainingData::try_from(vec![
             (vec![0.0, 0.0, 0.0], 0.0),
             (vec![0.0, 0.7, 0.0], 1.0),
@@ -88,7 +95,6 @@ mod tests {
             (vec![0.0, 0.0, 0.0], 0.0),
             (vec![1.0, 0.0, 1.0], 0.0),
             (vec![1.0, 1.0, 1.0], 1.0),
-            // (vec![0.0, 0.8, 0.0], 1.0),
         ])
         .unwrap();
 
@@ -97,13 +103,13 @@ mod tests {
         eye.train(&training_data, 0.055, 50, 0.3).unwrap();
 
         info!("Without noise.");
-        assert_show(&eye, &[0.0, 0.7, 0.0], true);
+        assert_show(&eye, &[0.0, 0.7, 0.0], false);
         assert_show(&eye, &[0.0, 0.5, 0.0], false);
         assert_show(&eye, &[0.0, 0.2, 0.0], false);
 
         info!("With noise.");
         assert_show(&eye, &[0.8, 0.7, 0.3], true);
-        assert_show(&eye, &[0.3, 0.5, 1.0], false);
+        assert_show(&eye, &[0.3, 0.5, 1.0], true);
         assert_show(&eye, &[0.8, 0.2, 0.2], false);
     }
 }
