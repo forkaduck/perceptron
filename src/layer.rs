42b5483b6c510ea07c0727013100840034f34b63
forkaduck <48600374+forkaduck@users.noreply.github.com>
2024-04-17 14:19:33 +0200

    Added learn strength optimizer and new error mode

--- src/layer.rs
+++ src/layer.rs
@@ -1,4 +1,4 @@
-use log::{debug, info};
+use log::{debug, info, warn};
 use rand::prelude::*;
 use std::ops::Range;
 
@@ -8,7 +8,7 @@ use colored::Colorize;
 #[derive(Debug)]
 pub enum LayerError {
     OutOfIterations,
-    Oscillating,
+    ErrStabilized,
 }
 
 pub struct Layer {
@@ -54,34 +54,88 @@ impl Layer {
         data: &TrainingData,
         learn_strength: f64,
         iterations: i32,
-        err_margin: f64,
+        err_max: f64,
     ) -> Result<(), LayerError> {
+        let mut err_sum: [f64; 2] = [0.0, f64::MAX];
+
         for i in 0..iterations {
-            let mut err_sum = 0.0;
+            err_sum[0] = 0.0;
 
             for k in 0..data.inner.len() {
                 let err = data.inner[k].output - self.output(&data.inner[k].input).0;
-                err_sum += err;
+                err_sum[0] += err;
 
                 for y in 0..data.len() {
                     let delta = learn_strength * data.inner[k].input[y] * err as f64;
                     self.weights[y] += delta;
                 }
-
-                debug!(
-                    "LRN: @ {} -> err: {:.4} err_sum: {:.4}",
-                    i,
-                    err.to_string().bold(),
-                    err_sum.to_string().red(),
-                );
             }
+            debug!(
+                "LRN: @ {} -> err_sum: {:.4}",
+                i,
+                err_sum[0].to_string().red(),
+            );
 
-            // Normal exit (Nears err_margin)
-            if err_sum < err_margin && err_sum > -err_margin {
-                debug!("Done learning!");
+            // Normal exit (err_sum is in range of err_margin)
+            if err_sum[0].abs() < err_max {
                 return Ok(());
             }
+
+            // Detect if the err_sum is not in range of err_margin and
+            // stopped shrinking.
+            if (err_sum[0] * 100.0).round() == (err_sum[1] * 100.0).round() {
+                return Err(LayerError::ErrStabilized);
+            }
+
+            err_sum[1] = err_sum[0];
         }
         Err(LayerError::OutOfIterations)
+    }
+
+    pub fn train_optimizer(
+        &mut self,
+        data: &TrainingData,
+        learn_range: Range<f64>,
+    ) -> Result<(), LayerError> {
+        let mut learn_strength: [f64; 2] = [learn_range.start, 0.0];
+        let mut err_sum: [f64; 2] = [0.0, f64::MAX];
+
+        loop {
+            // Try training with the current learn_strength.
+            debug!("Trying learn_strength: {:.6}", learn_strength[0]);
+
+            if let Err(e) = self.train(data, learn_strength[0], 50, 0.1) {
+                debug!("Learning error: {:?}", e);
+
+                if learn_strength[0] >= learn_range.end {
+                    return Err(e);
+                }
+            }
+
+            // Compare outputs of the layer with the training data.
+            err_sum[0] = 0.0;
+            for i in &data.inner {
+                let result = self.output(&i.input);
+
+                info!(
+                    "Output Test: {:?} -> {:.6} ?= {} | Bo: {} ",
+                    &i.input, result.0, i.output, result.1
+                );
+
+                err_sum[0] += (result.0 - i.output).abs();
+            }
+            debug!("Err: {:.6} prev: {:.6}", err_sum[0], err_sum[1]);
+
+            if err_sum[0] > err_sum[1] {
+                info!("Found optimum at {:.6}", learn_strength[1]);
+                self.train(data, learn_strength[1], 50, 0.3)?;
+                break Ok(());
+            }
+
+            learn_strength[1] = learn_strength[0];
+            learn_strength[0] += learn_range.start;
+
+            err_sum[1] = err_sum[0];
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,4 +14,38 @@ fn main() {
         .init()
         .unwrap();
 
+    fn assert_ask(data: &Layer, input: &[f64], rv: bool) {
+        let result = data.output(input);
+        info!("{:?} -> {:.6} | {}", input, result.0, result.1);
+
+        assert_eq!(data.output(input).1, rv);
+    }
+
+    let training_data = TrainingData::try_from(vec![
+        (vec![0.0, 0.0, 0.0], 0.0),
+        (vec![0.0, 0.7, 0.0], 1.0),
+        (vec![0.0, 0.8, 0.0], 1.0),
+        (vec![1.0, 0.0, 1.0], 0.0),
+        (vec![1.0, 0.6, 0.0], 1.0),
+        (vec![0.0, 0.7, 1.0], 1.0),
+        (vec![0.0, 0.0, 0.0], 0.0),
+        (vec![1.0, 0.0, 1.0], 0.0),
+        (vec![1.0, 1.0, 1.0], 1.0),
+        // (vec![0.0, 0.8, 0.0], 1.0),
+    ])
+    .unwrap();
+
+    let mut eye = Layer::new(training_data.len(), 0.5, false);
+
+    eye.train_optimizer(&training_data, 0.005..0.3).unwrap();
+
+    info!("Without noise.");
+    assert_ask(&eye, &[0.0, 0.7, 0.0], true);
+    assert_ask(&eye, &[0.0, 0.5, 0.0], true);
+    assert_ask(&eye, &[0.0, 0.2, 0.0], false);
+
+    info!("With noise.");
+    assert_ask(&eye, &[0.8, 0.7, 0.3], true);
+    assert_ask(&eye, &[0.3, 0.5, 1.0], true);
+    assert_ask(&eye, &[0.8, 0.2, 0.2], false);
 }
