f02c93c53d500dc46de0ee20e04e9aecf4099c66
forkaduck <48600374+forkaduck@users.noreply.github.com>
2024-04-23 15:56:45 +0200

    Added duration timer to all tests.

--- src/test.rs
+++ src/test.rs
@@ -1,8 +1,11 @@
 #[cfg(test)]
 mod tests {
+    use colored::Colorize;
+    use log::info;
+    use std::time::Instant;
+
     use crate::training_data::TrainingData;
     use crate::Layer;
-    use log::info;
 
     fn setup() {
         match simple_logger::SimpleLogger::new()
@@ -13,8 +16,12 @@ mod tests {
             Ok(()) => {}
             Err(_) => {}
         };
+
+        // The test harness doesn't print "\n" after the test name.
+        println!("");
     }
 
+    // Helper-function which outputs and checks the layer output.
     fn assert_show(data: &Layer, input: &[f64], rv: bool) {
         let result = data.output(input);
         info!("{:?} -> {:.6} | {}", input, result.0, result.1);
@@ -22,11 +29,20 @@ mod tests {
         assert_eq!(data.output(input).1, rv);
     }
 
+    // Helper-function for easier timing because --report-time is to inaccurate (and unstable).
+    fn timer_end(start: Instant) {
+        println!(
+            "Time elapsed: {}s",
+            (Instant::now() - start).as_secs_f64().to_string().green()
+        );
+    }
+
     /// Showcases that a one-dimensional layer can't learn
     /// an XOR Gate.
     #[test]
     fn xor_impossibility_weak() {
         setup();
+        let start = Instant::now();
 
         let training_data = TrainingData::try_from(vec![
             (vec![0.0, 0.0], 0.0),
@@ -44,6 +60,8 @@ mod tests {
         assert_show(&eye, &[0.0, 1.0], false);
         assert_show(&eye, &[1.0, 0.0], false);
         assert_show(&eye, &[1.0, 1.0], true);
+
+        timer_end(start);
     }
 
     /// Shows how multiple iterations with the same data
@@ -51,6 +69,7 @@ mod tests {
     #[test]
     fn optimizer_complex_patterns() {
         setup();
+        let start = Instant::now();
 
         let training_data = TrainingData::try_from(vec![
             (vec![0.0, 0.0, 0.0], 0.0),
@@ -80,12 +99,16 @@ mod tests {
         assert_show(&eye, &[0.8, 0.7, 0.3], true);
         assert_show(&eye, &[0.3, 0.5, 1.0], true);
         assert_show(&eye, &[0.8, 0.2, 0.2], false);
+
+        timer_end(start);
     }
 
     /// Shows the anti-proof of optimizer_complex_patterns.
     #[test]
     fn optimizer_proof() {
         setup();
+        let start = Instant::now();
+
         let training_data = TrainingData::try_from(vec![
             (vec![0.0, 0.0, 0.0], 0.0),
             (vec![0.0, 0.7, 0.0], 1.0),
@@ -112,5 +135,7 @@ mod tests {
         assert_show(&eye, &[0.8, 0.7, 0.3], true);
         assert_show(&eye, &[0.3, 0.5, 1.0], true);
         assert_show(&eye, &[0.8, 0.2, 0.2], false);
+
+        timer_end(start);
     }
 }
