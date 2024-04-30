84379aba8e7c1b549e5033b0f3fb9c928ef7919c
forkaduck <48600374+forkaduck@users.noreply.github.com>
2024-04-30 18:18:51 +0200

    Added a basic output function to the group struct

--- src/group.rs
+++ src/group.rs
@@ -1,20 +1,24 @@
 use log::info;
 
+mod test;
+
 use crate::layer::Layer;
 
 pub struct Group {
     layers: Vec<Vec<Layer>>,
+    input_length: usize,
 }
 
 impl Group {
     pub fn new(depth: u32, inputs: usize, random: bool) -> Self {
         let mut group = Group {
             layers: Vec::with_capacity(depth as usize),
+            input_length: inputs,
         };
 
         let mut counter: usize = 0;
 
-        for i in 0..depth {
+        for i in (0..depth).rev() {
             let iterations = inputs.pow(i);
             let mut temp: Vec<Layer> = Vec::with_capacity(iterations);
 
@@ -28,5 +32,21 @@ impl Group {
 
         info!("Layers allocated: {}", counter);
         group
+    }
+
+    pub fn output(&self, input: &[f64]) -> Vec<f64> {
+        let mut temp_inputs: Vec<f64> = Vec::from(input);
+        let mut temp_outputs: Vec<f64> = Vec::with_capacity(input.len());
+
+        for i in &self.layers {
+            temp_outputs.clear();
+
+            for (index, k) in i.into_iter().enumerate() {
+                let offset = index * self.input_length;
+                temp_outputs.push(k.output(&temp_inputs[offset..offset + self.input_length]).0);
+            }
+            temp_inputs = temp_outputs.clone();
+        }
+        temp_inputs
     }
 }
